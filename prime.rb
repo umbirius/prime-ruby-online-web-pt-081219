b9ae6960b6fbae20d75d78591aed7799b1cf3e36
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-18 20:09:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+require "pry"
+
+def prime?(num)
+  i=1
+  divis = []
+if num >= 0 
+while i < num
+  if num % i == 0 
+    divis << i 
+  end 
+end 
+binding.pry
+else false 
+end 
+
+if divis == [1,num]
+  puts true 
+else
+  puts false
+end 
+end 
+
+prime?(8)
